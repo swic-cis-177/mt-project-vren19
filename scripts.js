b8cdf9058f68a36f969f2219afb5aab3d4deca94
Tori Engel <victoriaengel19@gmail.com>
2020-10-09 22:37:50 -0500

    FIX Form stops accepting submissions after 10.

--- scripts.js
+++ scripts.js
@@ -3,14 +3,14 @@ const tileNumber = [12, 12, 12, 12, 12, 12, 12, 12, 12];
 const roundNumber = document.querySelector("title");
 const form = document.querySelector("form");
 let round = 1;
-
+let text = "Round 1";
 //if (1<=round<=10) {
 form.addEventListener("submit", function () {
   //prevent default browser permission
   event.preventDefault();
   //action when submit button is clicked.
-  const formData = {};
-  if (1 <= round < 10) {
+  if (round < 10) {
+    const formData = {};
     for (let i = 0; i <= 8; i++) {
       //print amount of each tile pulled
       formData[event.target.elements[i].id] = event.target.elements[i];
@@ -32,11 +32,12 @@ form.addEventListener("submit", function () {
       );
     }
     round++;
+    text = "Round " + round;
     console.log(round);
-
-    //TODO: change round number up top
-    //TODO: reset number boxes
+    document.getElementById("round").innerHTML = text;
   } else {
     console.log("Game Over.");
+    document.getElementById("round").innerHTML = "Game Over";
   }
+  //TODO: reset number boxes
 });
