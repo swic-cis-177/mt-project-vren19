6be48342feefe80771e131bc5cda12a98fc0aa16
Tori Engel <victoriaengel19@gmail.com>
2020-10-09 19:58:24 -0500

    add tileNumber array, tilesPulled, console.log(tiles pulled), console.log (tilesleft)

--- scripts.js
+++ scripts.js
@@ -1,26 +1,33 @@
+//set starting number of each tile
+const tileNumber = [12, 12, 12, 12, 12, 12, 12, 12, 12];
+
 const form = document.querySelector("form");
 
 form.addEventListener("submit", function () {
   //prevent default browser permission
   event.preventDefault();
-
-  //print the amount of tiles pulled of each tile.
+  //action when submit button is clicked.
   const formData = {};
   for (let i = 0; i <= 8; i++) {
+    //print amount of each tile pulled
     formData[event.target.elements[i].id] = event.target.elements[i];
-    console.log(event.target.elements[i].value);
+    let tilesPulled = event.target.elements[i].value;
+
+    console.log(
+      "The number of " +
+        event.target.elements[i].id +
+        "s pulled: " +
+        event.target.elements[i].value
+    );
+
+    //print the amount of each tile left
+    tileNumber[i] = tileNumber[i] - tilesPulled;
+
+    console.log(
+      "The number of " +
+        event.target.elements[i].id +
+        "s left: " +
+        tileNumber[i]
+    );
   }
 });
-
-/**
- * {Neutron Bomb: event.target.elements[0].value
- * Gauss Shield: event.target.elements[1].value
- * nanorobots: event.target.elements[2].value
- * starbase: event.target.elements[3].value
- * improved hull:event.target.elements[4].value
- * Fusion Drive: event.target.elements[5].value
- * Plasma Cannon: event.target.elements[6].value
- * Fusion Source: event.target.elements[7].value
- * Advanced Robotics: event.target.elements[8].value
- * }
- */
