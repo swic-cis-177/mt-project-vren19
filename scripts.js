ae29930e64178ff8b3e5478f7ed1ab5bbb4dd165
Tori Engel <victoriaengel19@gmail.com>
2020-10-09 22:25:16 -0500

    Troubleshooting still - still allowing for more than ten submissions. Not resetting.

--- scripts.js
+++ scripts.js
@@ -2,14 +2,15 @@
 const tileNumber = [12, 12, 12, 12, 12, 12, 12, 12, 12];
 const roundNumber = document.querySelector("title");
 const form = document.querySelector("form");
-
 let round = 1;
-do {
-  form.addEventListener("submit", function () {
-    //prevent default browser permission
-    event.preventDefault();
-    //action when submit button is clicked.
-    const formData = {};
+
+//if (1<=round<=10) {
+form.addEventListener("submit", function () {
+  //prevent default browser permission
+  event.preventDefault();
+  //action when submit button is clicked.
+  const formData = {};
+  if (1 <= round < 10) {
     for (let i = 0; i <= 8; i++) {
       //print amount of each tile pulled
       formData[event.target.elements[i].id] = event.target.elements[i];
@@ -18,7 +19,7 @@ do {
         "The number of " +
           event.target.elements[i].id +
           "s pulled: " +
-          event.target.elements[i].value
+          tilesPulled
       );
       //print the amount of each tile left
       tileNumber[i] = tileNumber[i] - tilesPulled;
@@ -29,16 +30,13 @@ do {
           "s left: " +
           tileNumber[i]
       );
-      round++;
-      console.log(round);
-      //document.input.reset();
-
-      //TODO: change round number up top
-      //document.labelID('tiles').value = null;
-
-      ///TODO: reset number boxes
-      //roundNumber.textContent = "Round Number" + round;
     }
-  });
-} while (round <= 10);
-console.log("Game Over.");
+    round++;
+    console.log(round);
+
+    //TODO: change round number up top
+    //TODO: reset number boxes
+  } else {
+    console.log("Game Over.");
+  }
+});
