d697ab95d0f55d1d09e0452414a690c8e1a22cff
Tori <victoriaengel19@gmail.com>
2020-12-06 21:43:08 -0600

    Merge Feedback

---
+++ data.js
@@ -0,0 +1,11 @@
+export default [
+  { tile: "Neutron Bomb", bag: 12, board: 0 },
+  { tile: "Gauss Shield", bag: 12, board: 0 },
+  { tile: "Nanorobots", bag: 12, board: 0 },
+  { tile: "Starbase", bag: 12, board: 0 },
+  { tile: "Improved Hull", bag: 12, board: 0 },
+  { tile: "Fusion Drive", bag: 12, board: 0 },
+  { tile: "Plasma Cannon", bag: 12, board: 0 },
+  { tile: "Fusion Source", bag: 12, board: 0 },
+  { tile: "Advanced Robotics", bag: 12, board: 0 },
+];
