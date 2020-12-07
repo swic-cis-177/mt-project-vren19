5f23d6a63d4e54bd9bb8e20353b9152dfb0fa7a6
Tori <victoriaengel19@gmail.com>
2020-12-07 00:56:35 -0600

    Move functions to library

--- scripts.js
+++ scripts.js
@@ -1,58 +1,13 @@
-import { genNumberTiles } from "./lib.js";
+import data from "./data.js";
+import { createTileTable, formSubmitted } from "./lib.js";
+//create tile table to replace console log
+createTileTable(data);
 
-const tileNumbers = genNumberTiles();
-
-const roundNumber = document.querySelector("title");
 const form = document.querySelector("form");
-let round = 1;
-let text = "Round 1";
 //if (1<=round<=10) {
 form.addEventListener("submit", function () {
   //prevent default browser permission
   event.preventDefault();
   //action when submit button is clicked.
-  if (round < 10) {
-    const formData = {};
-    console.log("Beginning of Round " + round);
-    for (let i = 0; i <= 8; i++) {
-      //print amount of each tile pulled
-      formData[event.target.elements[i].id] = event.target.elements[i];
-      let tilesPulled = event.target.elements[i].value;
-      console.log(
-        "The number of " +
-          event.target.elements[i].id +
-          " tile(s) pulled: " +
-          tilesPulled
-      );
-      //print the amount of each tile left
-      tileNumbers[i] = tileNumbers[i] - tilesPulled;
-
-      console.log(
-        "The number of " +
-          event.target.elements[i].id +
-          " tile(s) left: " +
-          tileNumbers[i]
-      );
-    }
-    console.log("End of Round " + round);
-    //progress to next round
-    round++;
-    text = "Round " + round;
-    document.getElementById("round").innerHTML = text;
-    //reset values for inputs eventually get more efficient way to do this if it exists.
-    document.getElementById("neutronbomb").value = 0;
-    document.getElementById("gaussShield").value = 0;
-    document.getElementById("nanoRobots").value = 0;
-    document.getElementById("starbase").value = 0;
-    document.getElementById("improvedHull").value = 0;
-    document.getElementById("fusionDrive").value = 0;
-    document.getElementById("plasmaCannon").value = 0;
-    document.getElementById("fusionSource").value = 0;
-    document.getElementById("advancedRobotics").value = 0;
-  }
-  //End form submission when game is over
-  else {
-    console.log("Game Over.");
-    document.getElementById("round").innerHTML = "Game Over";
-  }
+  formSubmitted();
 });
